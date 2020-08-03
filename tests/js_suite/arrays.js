ca00e435de2697d72beb752b6d795da89ea273c3
Rohit Awate <rohitawate121@gmail.com>
2020-08-03 11:38:38 +0530

    Add array deref tests and make libcurt shared

--- tests/js_suite/arrays.js
+++ tests/js_suite/arrays.js
@@ -1,8 +1,13 @@
 let homoList = ["apples", "oranges", "watermelons"]; 
 let heteroList = [1, 2, "hello", true];
+console.log(heteroList[1])
+console.log(heteroList[2]);
 
 // Array of objects
 let objects = [{name: "Rohit", age: 22}, {name: "Peter", age: 19}];
+console.log(objects[0].name);
+console.log(objects[0]["name"]);
+console.log(objects[0]['name']);
 
 // homogenous arrays in objects
 let colors = {
@@ -10,6 +15,8 @@ let colors = {
 	"secondary": ["green", "orange", "purple"],
 	"tertiary": ["yellow-orange", "red-orange", "red-purple", "blue-purple", "blue-green", "yellow-green"],
 };
+console.log(colors["primary"][1]);
+console.log(colors['tertiary'][2]);
 
 // superhero name: real name in single quotes, is billionaire, 1 for Marvel/2 for DC
 let superheroes = {
@@ -18,6 +25,8 @@ let superheroes = {
 	"Spiderman": ['Peter Parker', false, 2],
 	"Superman": ['Clark Kent', false, 2]
 };
+console.log(superheroes["Batman"]);
+console.log(superheroes['Superman']);
 
 // Multi-dimensional arrays
 const matrix = [
@@ -25,3 +34,5 @@ const matrix = [
 	[4, 5, 6],
 	[7, 8, 9]
 ];
+console.log(matrix[0][0]);
+console.log(matrix[2][2]);
